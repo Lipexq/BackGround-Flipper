8ad6a4083f63e78f9fb2363e4fd000e1f33e5d33
Filipe <18fiale@gmail.com>
2022-07-09 09:59:24 -0300

    main | add delet color and add color

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,17 +1,37 @@
 let arrayName = ['#c8a2c8', '#0000FF', '#808080', '#008000', '#FF0000'];
 
-var count = 0;
+var count = 1;
 
 function Switch() {
 
     if (count < arrayName.length) {
         document.getElementById('corpo').style.backgroundColor = arrayName[count];
-        document.getElementById('selection').innerHTML = "Cor atual " + arrayName[count];
+        document.getElementById('selection').innerHTML = "ID da cor = " + count + '<br/>' + "Cor atual " + arrayName[count];
         count++;
+
+    } else {
+        count = 1;
+        let newColor = prompt("Deseja adicionar alguma cor? yes or not");
+
+        if (newColor == 'yes') {
+            let Color = prompt("Digite a cor em Hexadecimal por favor");
+            arrayName.unshift(Color);
+            count = 1;
+
+        } else {
+            let question = prompt("Deseja Apagar alguma cor? yes or not");
+
+            if (question == 'yes') {
+               var delets = prompt("Digite o ID da cor a ser apagada");
+                arrayName.splice(delets-1, 1);
+            } else {
+                alert("blz bb então nao apaga nada (y) chatao hein");
+            }
+        }
     }
-    else {
-        let newColor = prompt('Deseja Adicionar Alguma Cor? HexaDecimal Por favor');
-        arrayName.unshift(newColor);
-        count = 0;
-    }
+}
+
+
+function random() {
+    var randomizar = arrayName[Math.floor(Math.random() * arrayName.length)];
 }
