82876898317bf59ec569d5a71b32c593e3bbc539
Filipe <18fiale@gmail.com>
2022-07-13 22:48:08 -0300

    main | add more fuctions and rows

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,59 +1,104 @@
-let arrayName = ['purple', 'yellow', 'brown', 'orange'];
-// let objects = [
-//     {Color: 'purple', ID: '1'}
-// ]
-
-
 var count = 0;
+var countback = 0;
 
-function Switch() {
+var arrayColor = [
+    {
+        ID: 1,
+        Color: 'Purple'
+    },
 
-    if (count < arrayName.length) {
-        document.getElementById('corpo').style.backgroundColor = arrayName[count];
-        // (count>0) ? document.getElementById('').innerHTML = "id color =" = + (count-1) + '<br/>' + 'Cor atual: ' + arrayName[count] : count;
-        document.getElementById('selection').innerHTML = "id color = " + (count + 1) + '<br/>' + 'Cor atual: ' + arrayName[count];
-        count++;
+    {
+        ID: 2,
+        Color: 'yellow'
+    },
 
-    } else {
-        count = 0;
+    {
+        ID: 3,
+        Color: 'Brown'
+    },
+
+    {
+        ID: 4,
+        Color: 'orange'
+    }
+]
+
+//Função de passar as cores
+function go() {
+    if (count <= arrayColor.length) {
+        document.getElementById('corpo').style.backgroundColor = arrayColor[count]['Color'];
+        document.getElementById('selection').innerHTML = 'Cor: ' + arrayColor[count]['Color'];
+        count++
+        countback++
+        if (count >= arrayColor.length) {
+            count = 0;
+            countback = 0;
+        }
     }
 }
 
+function back() {
+    if (countback < 1000) {
+        if (countback == 0) {
+            countback = arrayColor.length
+        }
+        document.getElementById('corpo').style.backgroundColor = arrayColor[countback - 1]['Color'];
+        document.getElementById('selection').innerHTML = 'Cor: ' + arrayColor[countback - 1]['Color'];
+        countback--
+    }
+}
+
+//adicionar cores
 function addColor() {
-    let Color = prompt("Digite uma cor em inglês");
+    let receive = prompt("Digite uma COR");
 
-    arrayName.unshift(Color);
-    count = 0;
-
-    if (Color == '') {
-        alert("selecione uma cor valida!");
-        count = 0;
-    }
-
-    return Switch();
-}
-
-function delet() {
-    let delets = prompt("digite o nome da cor a ser deletada");
-
-    if (delets.match(arrayName[delets])) {
-        let deleti = parseInt(prompt("Digite o ID da cor a ser apagada"));
-        arrayName.splice(deleti - 1, 1);
-        count = 0;
-        alert('cor apagada com sucesso!')
-        return Switch();
-
+    if (receive != '') {
+             arrayColor.push({ ID: arrayColor.length + 1, Color: receive })
+            count = arrayColor.length - 1;
+            alert('Cor adicionada com sucesso!')
     } else {
-        alert("cor invalida vei");
-        count = 0;
-        return Switch();
+        alert('digite uma cor valida');
     }
 }
 
-function shuffleArray() {
-    arrayName.sort();
-    alert("Cores em ordem alfabeticas com sucesso!");
-    count = 0;
-    return Switch();
+function remove() {
+    var delet = prompt("Deletar cor digite o nome");
+    if (delet != '') {
+        for (let i = 0; i < arrayColor.length; i++) {
 
+            if (arrayColor[i]['Color'] == delet) {
+                arrayColor.splice(i, 1);
+                count = 0
+                alert('Cor Apagada com sucesso!')
+
+            }
+        }
+    } else {
+        alert('Digita uma cor valida ae bixo')
     }
+}
+
+// Randomizar array
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+
+    while (currentIndex != 0) {
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+    alert("Array randomizado com sucesso")
+    count = 0;
+}
+
+//Mostrar o array atual
+function displays(){
+    document.getElementById('selection').innerHTML = '';
+    for(let j = 0; j < arrayColor.length; j++){
+        document.getElementById('selection').innerHTML += '[ ' + arrayColor[j]['Color'] + ' ]';
+    }
+};
