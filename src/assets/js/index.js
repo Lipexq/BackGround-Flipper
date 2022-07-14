490bd91f503a3a1d77b84af2d33f0f774b0f1b50
Filipe <18fiale@gmail.com>
2022-07-14 02:44:16 -0300

    main | add model

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,33 +1,36 @@
 var count = 0;
 var countback = 0;
+const modal = document.querySelector('.modal-container')
+var take = "";
+
 
 var arrayColor = [
-    {
-        ID: 1,
-        Color: 'Purple'
-    },
+    { ID: 1, Color: 'Purple' },
 
-    {
-        ID: 2,
-        Color: 'yellow'
-    },
+    { ID: 2, Color: 'yellow' },
 
-    {
-        ID: 3,
-        Color: 'Brown'
-    },
+    { ID: 3, Color: 'Brown' },
 
-    {
-        ID: 4,
-        Color: 'orange'
-    }
+    { ID: 4, Color: 'orange' }
 ]
+
+function takes(string){
+    modal.classList.add('active');
+    take = document.getElementById('color-valor').value;
+
+    document.getElementById('a').innerHTML = '';
+    for (let j = 0; j < arrayColor.length; j++) {
+        document.getElementById('a').innerHTML += '  { ' + arrayColor[j]['Color'] + ' }';
+    }
+    
+    return take;
+}
 
 //Função de passar as cores
 function go() {
     if (count <= arrayColor.length) {
         document.getElementById('corpo').style.backgroundColor = arrayColor[count]['Color'];
-        document.getElementById('selection').innerHTML = 'Cor: ' + arrayColor[count]['Color'];
+        document.getElementById('selection').innerHTML = arrayColor[count]['Color'];
         count++
         countback++
         if (count >= arrayColor.length) {
@@ -53,24 +56,22 @@ function addColor() {
     let receive = prompt("Digite uma COR");
 
     if (receive != '') {
-             arrayColor.push({ ID: arrayColor.length + 1, Color: receive })
-            count = arrayColor.length - 1;
-            alert('Cor adicionada com sucesso!')
+        arrayColor.push({ ID: arrayColor.length + 1, Color: receive })
+        count = arrayColor.length - 1;
+        alert('Cor adicionada com sucesso!')
     } else {
         alert('digite uma cor valida');
     }
 }
 
 function remove() {
-    var delet = prompt("Deletar cor digite o nome");
-    if (delet != '') {
+    take = document.getElementById('color-valor').value;
+    if (take != '') {
         for (let i = 0; i < arrayColor.length; i++) {
-
-            if (arrayColor[i]['Color'] == delet) {
+            if (arrayColor[i]['Color'] == take) {
                 arrayColor.splice(i, 1);
                 count = 0
                 alert('Cor Apagada com sucesso!')
-
             }
         }
     } else {
@@ -87,7 +88,6 @@ function shuffle(array) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
 
-        // And swap it with the current element.
         [array[currentIndex], array[randomIndex]] = [
             array[randomIndex], array[currentIndex]];
     }
@@ -95,10 +95,19 @@ function shuffle(array) {
     count = 0;
 }
 
-//Mostrar o array atual
-function displays(){
+// Mostrar o array atual
+function displays() {
     document.getElementById('selection').innerHTML = '';
-    for(let j = 0; j < arrayColor.length; j++){
-        document.getElementById('selection').innerHTML += '[ ' + arrayColor[j]['Color'] + ' ]';
+    for (let j = 0; j < arrayColor.length; j++) {
+        document.getElementById('selection').innerHTML += ' { ' + arrayColor[j]['Color'] + ' } ';
     }
 };
+
+//Modal
+function openModal() {
+    modal.classList.add('active')
+  }
+  
+  function closeModal() {
+    modal.classList.remove('active')
+  }
